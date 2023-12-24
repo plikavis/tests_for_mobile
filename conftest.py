5f90de1e8f9ca204f18ac568f166c3787afb5ef1
Polina Vishnyakova <vishnyakovapv@gmail.com>
2023-12-24 22:57:02 +0400

    remote comments

--- conftest.py
+++ conftest.py
@@ -2,7 +2,6 @@ import allure
 import pytest
 import requests
 from allure import step
-import allure_commons._allure
 from allure_commons._allure import StepContext
 from appium.options.android import UiAutomator2Options
 from appium import webdriver
@@ -17,25 +16,6 @@ import os
 def load_env():
     load_dotenv()
 
-
-# def attach_bstack_video(session_id):
-#     import requests
-#     bstack_session = requests.get(
-#         f'https://api.browserstack.com/app-automate/sessions/{session_id}.json',
-#         auth=({login}, {password}),
-#     ).json()
-#     print(bstack_session)
-#     video_url = bstack_session['automation_session']['video_url']
-#
-#     allure.attach(
-#         '<html><body>'
-#         '<video width="100%" height="100%" controls autoplay>'
-#         f'<source src="{video_url}" type="video/mp4">'
-#         '</video>'
-#         '</body></html>',
-#         name='video recording',
-#         attachment_type=allure.attachment_type.HTML,
-#     )
 
 @pytest.fixture(scope='session', autouse=True)
 def mobile_management():
@@ -97,7 +77,7 @@ def mobile_management():
     bstack_session = requests.get(url=f'https://api-cloud.browserstack.com/app-automate/sessions/{session_id}.json',
                                   auth=(login, password),
     ).json()
-    print(bstack_session)
+
     video_url = bstack_session['automation_session']['video_url']
 
     allure.attach(
